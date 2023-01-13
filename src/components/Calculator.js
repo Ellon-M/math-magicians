076df779e4f9f92a017065c6d6d991781a7e2e23
ellon <mordecaimwema11@gmail.com>
2023-01-13 19:30:47 +0300

    add handleclick event to calculator buttons, implement calculator logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,65 +1,81 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
+  handleClick = (e) => {
+    const buttonName = e.target.getAttribute('data-value');
+    const initState = this.state;
+    this.setState(calculate(initState, buttonName));
+  };
+
   render() {
+    const obj = this.state;
     return (
       <div className="container">
         <div className="calculator">
           <div className="input-container">
-            <span>121</span>
+            <span>
+              {obj.total}
+              {obj.operation}
+              {obj.next}
+            </span>
           </div>
           <div className="keys">
             <div className="row">
               <div className="number">
-                <span>AC</span>
-                <span>+/-</span>
-                <span>%</span>
+                <button data-value="AC" type="button" onClick={this.handleClick}>AC</button>
+                <button data-value="+/-" type="button" onClick={this.handleClick}>+/-</button>
+                <button data-value="%" type="button" onClick={this.handleClick}>%</button>
                 <div className="symbol">
-                  <span>x</span>
+                  <button data-value="x" type="button" onClick={this.handleClick}>x</button>
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="number">
-                <span>7</span>
-                <span>8</span>
-                <span>9</span>
+                <button data-value="7" type="button" onClick={this.handleClick}>7</button>
+                <button data-value="8" type="button" onClick={this.handleClick}>8</button>
+                <button data-value="9" type="button" onClick={this.handleClick}>9</button>
                 <div className="symbol">
-                  <span>÷</span>
+                  <button data-value="÷" type="button" onClick={this.handleClick}>÷</button>
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="number">
-                <span>4</span>
-                <span>5</span>
-                <span>6</span>
+                <button data-value="4" type="button" onClick={this.handleClick}>4</button>
+                <button data-value="5" type="button" onClick={this.handleClick}>5</button>
+                <button data-value="6" type="button" onClick={this.handleClick}>6</button>
                 <div className="symbol">
-                  <span>-</span>
+                  <button data-value="-" type="button" onClick={this.handleClick}>-</button>
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="number">
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
+                <button data-value="1" type="button" onClick={this.handleClick}>1</button>
+                <button data-value="2" type="button" onClick={this.handleClick}>2</button>
+                <button data-value="3" type="button" onClick={this.handleClick}>3</button>
                 <div className="symbol">
-                  <span>+</span>
+                  <button data-value="+" type="button" onClick={this.handleClick}>+</button>
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="number">
-                <span id="span-wide">0</span>
-                <span>.</span>
+                <button data-value="0" type="button" id="button-wide" onClick={this.handleClick}>0</button>
+                <button data-value="." type="button" onClick={this.handleClick}>.</button>
                 <div className="symbol">
-                  <span>=</span>
+                  <button data-value="=" type="button" onClick={this.handleClick}>=</button>
                 </div>
               </div>
             </div>
